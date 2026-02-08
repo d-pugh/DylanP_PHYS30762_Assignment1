da1cdab219fbc58d4c40bb81717774634f8d8dd6
d-pugh <dylan.pugh@student.manchester.ac.uk>
2026-02-08 21:02:11 +0000

    Added comments,

--- Dylan_Assignment-1.cpp
+++ Dylan_Assignment-1.cpp
@@ -1,28 +1,35 @@
 // Dylan Pugh 11020134
-// 04.02.2026
+// 08.02.2026
 // Assignment 1: Program to calculate transition energy using simple Bohr formula: E_ij = 13.6 * Z^2 * [1 / (n_j)^2 - 1/(n_i)^2] eV
 // inputs: Z, n_i, n_j (<n_i), units (J or eV)
 // outputs: E_ij
+// Task:  Ask the user to enter the atomic number, initial and final quantum numbers, and whether to 
+//        display the calculated energy of the transition in Joules or electron volts.
+//        After the calculated value is shown, ask whether to repeat (“y/n”) and stop on “n”
+//        Check at each stage for incorrect inputs (either in format or violating some physical conditions)
 
 #include<iostream>
 #include<iomanip>
 
 int main()
 {
+  //declare and initialise variables
   int n_i{1}; //initial energy level
   int n_j{1}; //final energy level 
   int Z{0}; //atomic number
 
+  //take inputs
   std::cout<<"Atomic number: ";
   std::cin>>Z;
   std::cout<<"Energy levels: ";
   std::cin>>n_i>>n_j;
-  
   std::cout<<"Z = "<<Z<<", n_i = "<<n_i<<", n_j = "<<n_j<<std::endl; // check inputs
 
+  //calculate E
   float E = 13.6 * Z*Z * (1/(n_j*n_j) - 1/(n_i*n_i));
 
-  std::cout<<"Transition energy = "<<E<<std::endl;
+  //output result
+  std::cout<<"Transition energy = "<<E<<" eV"<<std::endl;
 
   return 0;
 }
